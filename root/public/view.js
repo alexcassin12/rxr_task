2e220b84776906063bb6e0c9a94142d7de20597d
alexcassin12 <alex.cassin3@gmail.com>
2023-01-17 23:04:49 +0000

    rxr_test_step_2: Complete game

--- root/public/view.js
+++ root/public/view.js
@@ -1,4 +1,7 @@
+"use strict";
+
 class View {
+  // Text style for use on all texts displayed
   _textStyle = new PIXI.TextStyle({
     fontFamily: "Arial",
     fontSize: 20,
@@ -16,6 +19,7 @@ class View {
     wordWrapWidth: 440,
   });
 
+  // Creates the game
   createGame() {
     const app = new PIXI.Application({
       backgroundColor: 0xf1a8a8,
@@ -26,6 +30,8 @@ class View {
 
     return app;
   }
+
+  // Create all visual aspects of game
 
   createTimeRemainingTextField(app_) {
     const timeRemainingTextField = new PIXI.Graphics();
@@ -50,8 +56,8 @@ class View {
     return timeRemainingTextHeading;
   }
 
-  createTimeRemainingText(app_, textField_) {
-    const timeRemainingText = new PIXI.Text("1", this._textStyle);
+  createTimeRemainingText(app_, textField_, startTime_) {
+    const timeRemainingText = new PIXI.Text(startTime_, this._textStyle);
     textField_.addChild(timeRemainingText);
     timeRemainingText.x = app_.view.width / 2 - 10;
     timeRemainingText.y = 90;
@@ -87,11 +93,11 @@ class View {
     return scoreText;
   }
 
-  createColourCircle(app_) {
+  createColourCircle(app_, hex_) {
     const colourCircle = new PIXI.Graphics();
     colourCircle
-      .beginFill(0xd3d3d3, 1)
-      .lineStyle(2, 0x000000, 1)
+      .beginFill(hex_, 1)
+      .lineStyle(0, 0xffffff, 1)
       .drawCircle(app_.view.width / 2, 350, 100);
     app_.stage.addChild(colourCircle);
 
@@ -163,6 +169,67 @@ class View {
     return orangeCircle;
   }
 
+  createGameOverlay(app_) {
+    const overlay = new PIXI.Graphics();
+    overlay
+      .beginFill(0x000000, 0.7)
+      .drawRect(0, 0, app_.view.width, app_.view.height);
+    app_.stage.addChild(overlay);
+
+    return overlay;
+  }
+
+  createFinalScoreText(app_, overlay_, score_, totalSpins_) {
+    let text;
+    if (score_ === 0)
+      text = `Unlucky, you scored no points
+       from ${totalSpins_} attempts`;
+    if (score_ === 1)
+      text = `Not bad, you scored ${score_} point
+       from ${totalSpins_} attempts`;
+    if (score_ > 1 && score_ < 4)
+      text = `Not bad, you scored ${score_} points
+       from ${totalSpins_} attempts`;
+    if (score_ >= 4)
+      text = `Outstanding! You scored ${score_}
+       points from ${totalSpins_} attempts`;
+
+    const displayText = new PIXI.Text(text, this._textStyle);
+    displayText.x = app_.view.width / 4;
+    displayText.y = 80;
+    overlay_.addChild(displayText);
+  }
+
+  createReplayButton(app_, overlay_) {
+    const button = new PIXI.Graphics();
+    button
+      .beginFill(0xffffff, 1)
+      .lineStyle(2, 0xffffff, 1)
+      .drawCircle(app_.view.width / 2, 350, 100);
+    overlay_.addChild(button);
+
+    button.buttonMode = true;
+
+    return button;
+  }
+
+  createReplayButtonText(app_, button_) {
+    const text = new PIXI.Text(
+      `Click to 
+    Play Again`,
+      this._textStyle
+    );
+    text.x = app_.view.width / 2 - 80;
+    text.y = 310;
+    button_.addChild(text);
+  }
+
+  // Add event handlers to buttons
+  addReplayButtonHandler(button_, handler_) {
+    button_.interactive = true;
+    button_.on("pointerdown", () => handler_());
+  }
+
   addButtonHandlers(buttonsArr_, handler_) {
     buttonsArr_.forEach((b) => {
       b.interactive = true;
@@ -170,29 +237,30 @@ class View {
     });
   }
 
-  highlightSelection(btn_) {
-    console.log("highlighted");
+  // Methods to uodate/affect visuals currently renderede on screen
+
+  setCircleColour(app_, colourCircle_, colour_, borderPx_) {
+    colourCircle_
+      .clear()
+      .beginFill(colour_, 1)
+      .lineStyle(borderPx_, 0xffffff, 1)
+      .drawCircle(app_.view.width / 2, 350, 100);
   }
 
-  async cycleColours(app_, colourCircle_, responsesArr) {
-    const colours = responsesArr.map((r) => r.colourHex);
-    // Set colour of circle background to colours[0]
-    // wait 0.2 secs
-    // Set to next colour[1]
-
-    // Keep going for set amount of time
-  }
-
-  setResColour(app_, hexColour) {
-    // Set colour of circle to hex colour
+  cycleToNextColour(app_, colourCircle_, c_, i_) {
+    colourCircle_
+      .clear()
+      .beginFill(c_[i_], 1)
+      .lineStyle(1, 0x000000, 1)
+      .drawCircle(app_.view.width / 2, 350, 100);
   }
 
   updateScore(scoreText_, newScore_) {
-    // Update score from backend
+    scoreText_.text = newScore_;
   }
 
-  resetTime() {
-    // reset time display back to 20 secs
+  updateTime(text_, num_) {
+    text_.text = num_;
   }
 }
 
