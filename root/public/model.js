cab7de67f0ab681836ba936633ffa5f02d22991d
alexcassin12 <alex.cassin3@gmail.com>
2023-01-17 00:08:55 +0000

    rxr_test_step_1: Start writing game sequences and  planning out project using placeholders

---
+++ root/public/model.js
@@ -0,0 +1,13 @@
+"use strict";
+
+export const state = {
+  selectedColour: undefined,
+  responses: [
+    { colourString: "red", colourHex: 0xff0000 },
+    { colourString: "blue", colourHex: 0x0000ff },
+    { colourString: "green", colourHex: 0x00ff00 },
+    { colourString: "yellow", colourHex: 0xffff00 },
+    { colourString: "orange", colourHex: 0xffa500 },
+  ],
+  playerScore: 0,
+};
